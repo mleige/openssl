394a332da81f12678ba4fad6517824a5df29faf1
Todd Short <tshort@akamai.com>
2019-04-05 14:17:22 -0400

    Optimize session cache flushing
    Sort SSL_SESSION structures by timeout in the linked list.
    Iterate over the linked list for timeout, stopping when no more
    session can be flushed.
    Do SSL_SESSION_free() outside of SSL_CTX lock
    Update timeout upon use

--- ssl/ssl_local.h
+++ ssl/ssl_local.h
@@ -595,8 +595,8 @@ struct ssl_session_st {
      */
     long verify_result;         /* only for servers */
     CRYPTO_REF_COUNT references;
-    long timeout;
-    long time;
+    time_t timeout;
+    time_t time;
     unsigned int compress_meth; /* Need to lookup the method */
     const SSL_CIPHER *cipher;
     unsigned long cipher_id;    /* when ASN.1 loaded, this needs to be used to
@@ -635,6 +635,7 @@ struct ssl_session_st {
     unsigned char *ticket_appdata;
     size_t ticket_appdata_len;
     uint32_t flags;
+    SSL_CTX *owner;
     CRYPTO_RWLOCK *lock;
 };
 
